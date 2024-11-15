71a25e83447bd9edfc0d1426af22a1cb93413032
Mona Lisa <ass@ho.le>
2024-11-15 08:32:06 +0000

    Carousel Demo (shadcn/ui/carousel test task) + build for nginx server

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,24 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ui_shadcn_carousel_demo/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
